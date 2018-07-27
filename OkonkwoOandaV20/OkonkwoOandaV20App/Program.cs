ce1b70cac46a151da37c10b8da4e24e622b7f3ef
Osita Okonkwo <mrchrisok@hotmail.com>
2018-07-27 02:42:34 -0500

    added transaction stream usage to sample app

--- OkonkwoOandaV20/OkonkwoOandaV20App/Program.cs
+++ OkonkwoOandaV20/OkonkwoOandaV20App/Program.cs
@@ -2,42 +2,57 @@ using OkonkwoOandaV20;
 using OkonkwoOandaV20.Framework;
 using OkonkwoOandaV20.TradeLibrary.DataTypes.Communications;
 using OkonkwoOandaV20.TradeLibrary.DataTypes.Communications.Requests.Order;
+using OkonkwoOandaV20.TradeLibrary.DataTypes.Stream;
 using OkonkwoOandaV20.TradeLibrary.Primitives;
 using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace OkonkwoOandaV20App
 {
    class Program
    {
-      const string INSTRUMENT = InstrumentName.Currency.EURUSD;
-      static string _accountID = null;
-
       static void Main(string[] args)
       {
+         WriteNewLine("Hello trader! Welcome to OkonkwoOandaV20");
+
+         SetApiCredentials();
+
+         StartTransactionsStream();
+
          PutOnATrade().Wait();
+
+         StopTransactionsStream();
 
          Console.ReadKey();
       }
 
-      static private async Task PutOnATrade()
-      {
-         Console.WriteLine("Hello trader! Welcome to OkonkwoOandaV20");
+      static string AccountID { get; set; }
+      const string INSTRUMENT = InstrumentName.Currency.EURUSD;
 
-         // to start trading, first set your api credentials
+      static void SetApiCredentials()
+      {
+         WriteNewLine("Setting your V20 credentials ...");
+
+         AccountID = "101-001-1913854-002";
          var environment = EEnvironment.Practice;
          var token = "c9e9494d79013cac1d34f0e4dcb590cd-977a37b80762fb48cdb3b0b2e832628a";
-         var accountID = "101-001-1913854-002";
-         Credentials.SetCredentials(environment, token, accountID);
 
-         Console.WriteLine("Nice! Credentials are set. ...");
+         Credentials.SetCredentials(environment, token, AccountID);
 
-         // now, check the market status for EUR_USD
+         WriteNewLine("Nice! Credentials are set.");
+      }
+
+      static private async Task PutOnATrade()
+      {
+         WriteNewLine("Checking to see if EUR_USD is open for trading ...");
+
+         // first, check the market status for EUR_USD
          // if it is tradeable, we'll try to make some money :)
          if (!(await Utilities.IsMarketHalted(INSTRUMENT)))
          {
-            Console.WriteLine("Euro market is open and rockin', so let's start trading! ...");
+            WriteNewLine("EUR_USD is open and rockin', so let's start trading!");
 
             long? tradeID = await PlaceMarketOrder();
 
@@ -47,48 +62,50 @@ namespace OkonkwoOandaV20App
                // give it some time to make money :)
                await Task.Delay(10000);
 
+               WriteNewLine("Okay, we've waited 10 seconds. Closing trade now ...");
 
                // now, let' close the trade and collect our profits! .. hopefully
                TradeCloseResponse closeResponse = null;
                try
                {
-                  closeResponse = await Rest20.CloseTradeAsync(_accountID, tradeID.Value, "ALL");
+                  closeResponse = await Rest20.CloseTradeAsync(AccountID, tradeID.Value, "ALL");
                }
                catch
                {
-                  Console.WriteLine("Oops. The trade can't be closed. Something went wrong. :(");
+                  WriteNewLine("Oops. The trade can't be closed. Something went wrong. :(");
                }
 
                if (closeResponse != null)
                {
-                  Console.WriteLine("Nice! The trade is closed.");
+                  WriteNewLine("Nice! The trade is closed.");
 
                   var profit = closeResponse.orderFillTransaction.pl;
-                  Console.WriteLine($"Our profit was USD {profit}");
+                  WriteNewLine($"Our profit was USD {profit}");
 
-                  if(profit > 0)
-                     Console.WriteLine($"Nice work! You are an awesome trader.");
+                  if (profit > 0)
+                     WriteNewLine($"Nice work! You are an awesome trader.");
                   else
                   {
-                     Console.WriteLine($"Looks like you need to learn some money-making strategies. :(");
-                     Console.WriteLine($"Keep studying, learning, but most of all .. keep trading!!");
-                  }                
+                     WriteNewLine($"Looks like you need to learn some money-making strategies. :(");
+                     WriteNewLine($"Keep studying, learning, but most of all .. keep trading!!");
+                  }
                }
             }
          }
          else
          {
-            Console.WriteLine("Sorry, Oanda markets are closed or Euro market is not tradeable.");
-            Console.WriteLine("Try again another time.");
+            WriteNewLine("Sorry, Oanda markets are closed or Euro market is not tradeable.");
+            WriteNewLine("Try again another time.");
          }
 
       }
 
       static async Task<long?> PlaceMarketOrder(string side = "buy")
       {
-         _accountID = Credentials.GetDefaultCredentials().DefaultAccountId;
-         var oandaInstrument = (await Rest20.GetAccountInstrumentsAsync(_accountID, INSTRUMENT)).First();
-         long orderUnits = side == "buy" ? 1 : -1;
+         WriteNewLine("Creating a EUR_USD market BUY order ...");
+
+         var oandaInstrument = (await Rest20.GetAccountInstrumentsAsync(AccountID, INSTRUMENT)).First();
+         long orderUnits = side == "buy" ? 10 : -10;
 
          var request = new MarketOrderRequest(oandaInstrument)
          {
@@ -98,15 +115,56 @@ namespace OkonkwoOandaV20App
          OrderPostResponse response = null;
          try
          {
-            response = await Rest20.PostOrderAsync(_accountID, request);
-            Console.WriteLine("Congrats! You've put on a trade! Let it run! :)");
+            response = await Rest20.PostOrderAsync(AccountID, request);
+            WriteNewLine("Congrats! You've put on a trade! Let it run! :)");
          }
          catch
          {
-            Console.WriteLine("Oops. Order creation failed.");
+            WriteNewLine("Oops. Order creation failed.");
          }
 
          return response?.orderFillTransaction?.tradeOpened?.tradeID;
+      }
+
+      #region transactions stream
+      static Semaphore _transactionReceived;
+      static TransactionsSession _transactionsSession;
+
+      static void StartTransactionsStream()
+      {
+         WriteNewLine("Starting transactions stream ...");
+
+         _transactionsSession = new TransactionsSession(AccountID);
+         _transactionReceived = new Semaphore(0, 100);
+         _transactionsSession.DataReceived += OnTransactionReceived;
+
+         _transactionsSession.StartSession();
+
+         bool success = _transactionReceived.WaitOne(10000);
+
+         if (success)
+            WriteNewLine("Good news!. Transactions stream is functioning.");
+         else
+            WriteNewLine("Bad news!. Transactions stream is not functioning.");
+      }
+
+      protected static void OnTransactionReceived(TransactionStreamResponse data)
+      {
+         if (!data.IsHeartbeat())
+            WriteNewLine("V20 notification - New account transaction: " + data.transaction.type);
+
+         _transactionReceived.Release();
+      }
+
+      static void StopTransactionsStream()
+      {
+         _transactionsSession.StopSession();
+      }
+      #endregion
+
+      static void WriteNewLine(string message)
+      {
+         Console.WriteLine("\n" + message);
       }
    }
 }
